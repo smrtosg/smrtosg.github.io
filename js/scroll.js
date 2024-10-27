8265f4b1224880a8874f722692b211e467f9290d
SMRTO <weezyvil4@gmail.com>
2024-10-27 21:11:19 +0800

    2024-10 Cumulative Update for Version 2409

--- js/scroll.js
+++ js/scroll.js
@@ -5,5 +5,5 @@ function scrollIndicator() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
+  document.getElementById("scrollIndicator").style.width = scrolled + "%";
 }
