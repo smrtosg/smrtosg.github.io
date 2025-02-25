474f6f3928171209f6f43be8db38fd2379c705f9
nrfz <weezyvil4@gmail.com>
2025-02-25 12:06:27 +0800

    Update smrto-mrt.js

--- js/mkbf/smrto-mrt.js
+++ js/mkbf/smrto-mrt.js
@@ -50,10 +50,10 @@ const chartOptions = {
 const linesmrt = new Chart(smrtMRT, {
     type: 'line', // or 'bar', 'pie', etc.
     data: {
-        labels: ["2020", "2021", "2022", "2023", "Oct '23 - Dec '24"],
+        labels: ["2020", "2021", "2022", "2023", "2024"],
         datasets: [{
             label: "HBL",
-            data: [1082000, 2045000, 3075000, 3675000, 4725000],
+            data: [1082000, 2045000, 3075000, 3675000, 4951000],
             borderColor: 'rgb(255, 187, 0)',
             fill: true,
             backgroundColor: 'rgba(207, 61, 61,0.05)',
@@ -62,7 +62,7 @@ const linesmrt = new Chart(smrtMRT, {
 
         {
             label: "CGL",
-            data: [3363000, 2027000, 3020000, 3200000, 4050000],
+            data: [3363000, 2027000, 3020000, 3200000, 4450000],
             borderColor: 'rgb(0, 154, 68)',
             fill: true,
             backgroundColor: 'rgba(0, 154, 68,0.05)',
@@ -71,7 +71,7 @@ const linesmrt = new Chart(smrtMRT, {
 
         {
             label: "AEL",
-            data: [2365000, 3420000, 3802000, 3721000, 4080000],
+            data: [2365000, 3420000, 3802000, 3721000, 4750000],
             borderColor: 'rgb(39, 39, 157)',
             fill: true,
             backgroundColor: 'rgba(39, 39, 157,0.05)',
@@ -80,7 +80,7 @@ const linesmrt = new Chart(smrtMRT, {
 
         {
             label: "NWL",
-            data: [1733000, 1826000, 1840000, 1209000, 2350000],
+            data: [1733000, 1826000, 1840000, 1209000, 3210000],
             borderColor: 'rgb(14, 103, 44)',
             fill: true,
             backgroundColor: 'rgba(14, 103, 44,0.05)',
@@ -98,7 +98,7 @@ const linesmrt = new Chart(smrtMRT, {
 
         {
             label: "YSLRT",
-            data: [1131000, 2224000, 2452000, 2482000, 4030000],
+            data: [1131000, 2224000, 2452000, 2482000, 4530000],
             borderColor: 'rgb(112, 133, 115)',
             fill: true,
             backgroundColor: 'rgba(112, 133, 115,0.05)',
