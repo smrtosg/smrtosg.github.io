e0c99adeb00d8860764a7412b1616ff8f27c7f49
nurafianZulkifli <weezyvil4@gmail.com>
2026-06-09 21:45:22 +0800

    2026-05 Cumulative Update for Main (30526.3)

--- js/mkbf/smrto-mrt.js
+++ js/mkbf/smrto-mrt.js
@@ -50,10 +50,10 @@ const chartOptions = {
 const linesmrt = new Chart(smrtMRT, {
     type: 'line', // or 'bar', 'pie', etc.
     data: {
-        labels: ["2022", "2023", "2024", "2025", "Jan 2026"],
+        labels: ["2022", "2023", "2024", "2025", "May 2026"],
         datasets: [{
             label: "HBL",
-            data: [3075000, 3675000, 4951000, 6050000, 6350000],
+            data: [3075000, 3675000, 4951000, 6050000, 6070000],
             borderColor: 'rgb(255, 187, 0)',
             fill: true,
             backgroundColor: 'rgba(207, 61, 61,0.05)',
@@ -63,7 +63,7 @@ const linesmrt = new Chart(smrtMRT, {
 
         {
             label: "CGL",
-            data: [3020000, 3200000, 4450000, 6570000, 6800000],
+            data: [3020000, 3200000, 4450000, 6570000, 7000000],
             borderColor: 'rgb(0, 154, 68)',
             fill: true,
             backgroundColor: 'rgba(0, 154, 68,0.05)',
@@ -73,7 +73,7 @@ const linesmrt = new Chart(smrtMRT, {
 
         {
             label: "AEL",
-            data: [3802000, 3721000, 4750000, 8060000, 8500000],
+            data: [3802000, 3721000, 4750000, 8060000, 8200000],
             borderColor: 'rgb(39, 39, 157)',
             fill: true,
             backgroundColor: 'rgba(39, 39, 157,0.05)',
@@ -83,7 +83,7 @@ const linesmrt = new Chart(smrtMRT, {
 
         {
             label: "NWL",
-            data: [ 1840000, 1209000, 3210000, 7250000, 7500000],
+            data: [ 1840000, 1209000, 3210000, 7250000, 8200000],
             borderColor: 'rgb(14, 103, 44)',
             fill: true,
             backgroundColor: 'rgba(14, 103, 44,0.05)',
@@ -93,7 +93,7 @@ const linesmrt = new Chart(smrtMRT, {
 
         {
             label: "WTL",
-            data: [,,4503000, 7980000, 8005000],
+            data: [,,4503000, 7980000, 7805000],
             borderColor: 'rgb(33, 116, 217)',
             fill: true,
             backgroundColor: 'rgba(14, 103, 44,0.05)',
@@ -103,7 +103,7 @@ const linesmrt = new Chart(smrtMRT, {
 
         {
             label: "YSLRT",
-            data: [2452000, 2482000, 4530000, 7050000, 7200000],
+            data: [2452000, 2482000, 4530000, 7050000, 7700000],
             borderColor: 'rgb(112, 133, 115)',
             fill: true,
             backgroundColor: 'rgba(112, 133, 115,0.05)',
